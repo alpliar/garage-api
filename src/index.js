8670a34c31dd1f08029fb8c84aa77016767f9c9c
alpliar <al.pliar@gmail.com>
2020-10-22 14:29:50 +0200

    Adding Swagger and wrapping up.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-// Require external modules
-const mongoose = require('mongoose')
-
-// Connect to DB
-mongoose.connect('mongodb://localhost/mycargarage')
- .then(() => console.log('MongoDB connected…'))
- .catch(err => console.log(err))
-
-
-// Require the framework and instantiate it
-const fastify = require('fastify')({
-    logger: true
-})
+// Import server
+const fastify = require('./server.js')
   
 // Import Routes
 const routes = require('./routes')
+
+// Import Swagger Options
+const swagger = require('./config/swagger')
+
+// Register Swagger
+fastify.register(require('fastify-swagger'), swagger.options)
 
 // Loop over each route
 routes.forEach((route, index) => {
@@ -24,6 +19,7 @@ routes.forEach((route, index) => {
 const start = async () => {
 try {
     await fastify.listen(3000)
+    fastify.swagger()
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
 } catch (err) {
     fastify.log.error(err)
