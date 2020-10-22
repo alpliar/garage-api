cb2af3f7bd335932dec6727a1d95a6103f25cab6
alpliar <al.pliar@gmail.com>
2020-10-22 10:32:28 +0200

    connect to database

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+// Require external modules
+const mongoose = require('mongoose')
+
+// Connect to DB
+mongoose.connect('mongodb://localhost/mycargarage')
+ .then(() => console.log('MongoDB connected…'))
+ .catch(err => console.log(err))
+
 // Require the framework and instantiate it
 const fastify = require('fastify')({
     logger: true
