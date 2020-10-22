58d9d1f755da181221438f3387e278f60722c2f0
alpliar <al.pliar@gmail.com>
2020-10-22 13:41:03 +0200

    feat: Putting it all together and testing our API

---
+++ src/controllers/carController.js
@@ -0,0 +1,62 @@
+// External Dependancies
+const boom = require('@hapi/boom')
+
+// Get Data Models
+const Car = require('../models/Car')
+
+// Get all cars
+exports.getCars = async (req, reply) => {
+  try {
+    const cars = await Car.find()
+    return cars
+  } catch (err) {
+    throw boom.boomify(err)
+  }
+}
+
+// Get single car by ID
+exports.getSingleCar = async (req, reply) => {
+  try {
+    const id = req.params.id
+    const car = await Car.findById(id)
+    return car
+  } catch (err) {
+    throw boom.boomify(err)
+  }
+}
+
+// Add a new car
+exports.addCar = async (req, reply) => {
+  try {
+    console.log({...req.body})
+    const car = new Car({...req.body})
+    const newCar = await car.save()
+    return newCar
+  } catch (err) {
+    throw boom.boomify(err)
+  }
+}
+
+// Update an existing car
+exports.updateCar = async (req, reply) => {
+  try {
+    const id = req.params.id
+    const car = req.body
+    const { ...updateData } = car
+    const update = await Car.findByIdAndUpdate(id, updateData, { new: true })
+    return update
+  } catch (err) {
+    throw boom.boomify(err)
+  }
+}
+
+// Delete a car
+exports.deleteCar = async (req, reply) => {
+  try {
+    const id = req.params.id
+    const car = await Car.findByIdAndRemove(id)
+    return car
+  } catch (err) {
+    throw boom.boomify(err)
+  }
+}
--- src/index.js
+++ src/index.js
@@ -6,25 +6,28 @@ mongoose.connect('mongodb://localhost/mycargarage')
  .then(() => console.log('MongoDB connected…'))
  .catch(err => console.log(err))
 
+
 // Require the framework and instantiate it
 const fastify = require('fastify')({
     logger: true
-  })
+})
   
-  // Declare a route
-  fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
-  })
-  
-  // Run the server!
-  const start = async () => {
-    try {
-      await fastify.listen(3000)
-      fastify.log.info(`server listening on ${fastify.server.address().port}`)
-    } catch (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
-  }
-  start()
-  
+// Import Routes
+const routes = require('./routes')
+
+// Loop over each route
+routes.forEach((route, index) => {
+	fastify.route(route)
+})  
+
+// Run the server!
+const start = async () => {
+try {
+    await fastify.listen(3000)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+} catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+}
+}
+start()
