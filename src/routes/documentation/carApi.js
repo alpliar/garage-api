19eac5422c0f490d0a2e1a7d8e546ccfe8b08773
alpliar <al.pliar@gmail.com>
2020-10-22 14:50:58 +0200

    docs: update cars doc

--- src/routes/documentation/carApi.js
+++ src/routes/documentation/carApi.js
@@ -29,3 +29,87 @@ exports.addCarSchema = {
     }
   }
   
+  exports.getCarsSchema = {
+    description: 'Find all cars',
+    tags: ['cars'],
+    summary: 'Finds all cars',
+    response: {
+      200: {
+        description: 'Successful response',
+        type: 'array'
+      }
+    }
+  }
+
+  exports.getSingleCarSchema = {
+    description: 'Find a car',
+    tags: ['cars'],
+    summary: 'Finds a car with its id',
+    response: {
+      200: {
+        description: 'Successful response',
+        type: 'object',
+        properties: {
+          _id: { type: 'string' },
+          title: { type: 'string' },
+          brand: { type: 'string' },
+          price: { type: 'string' },
+          age: { type: 'number' },
+          services: { type: 'object' },
+          __v: { type: 'number' }
+        }
+      }
+    }
+  }
+
+  exports.updateCarSchema = {
+    description: 'Update a car',
+    tags: ['cars'],
+    summary: 'Finds a car with its id and updates it',
+    body: {
+        type: 'object',
+        properties: {
+          title: { type: 'string' },
+          brand: { type: 'string' },
+          price: { type: 'string' },
+          age: { type: 'number' },
+          services: { type: 'object' }
+        }
+      },
+    response: {
+      200: {
+        description: 'Successful response',
+        type: 'object',
+        properties: {
+          _id: { type: 'string' },
+          title: { type: 'string' },
+          brand: { type: 'string' },
+          price: { type: 'string' },
+          age: { type: 'number' },
+          services: { type: 'object' },
+          __v: { type: 'number' }
+        }
+      }
+    }
+  }
+
+  exports.deleteCarSchema = {
+    description: 'Delete a car',
+    tags: ['cars'],
+    summary: 'Finds a car with its id and delete it',
+    response: {
+      200: {
+        description: 'Successful response',
+        type: 'object',
+        properties: {
+          _id: { type: 'string' },
+          title: { type: 'string' },
+          brand: { type: 'string' },
+          price: { type: 'string' },
+          age: { type: 'number' },
+          services: { type: 'object' },
+          __v: { type: 'number' }
+        }
+      }
+    }
+  }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,33 +1,37 @@
 // Import our Controllers
 const carController = require('../controllers/carController')
+const documentation = require('./documentation/carApi')
 
 const routes = [
   {
     method: 'GET',
     url: '/api/cars',
-    handler: carController.getCars
+    handler: carController.getCars,
+    schema: documentation.getCarsSchema
   },
   {
     method: 'GET',
     url: '/api/cars/:id',
-    handler: carController.getSingleCar
+    handler: carController.getSingleCar,
+    schema: documentation.getSingleCarSchema
   },
   {
     method: 'POST',
     url: '/api/cars',
-    handler: carController.addCar
-    // ,
-    // schema: documentation.addCarSchema
+    handler: carController.addCar,
+    schema: documentation.addCarSchema
   },
   {
     method: 'PUT',
     url: '/api/cars/:id',
-    handler: carController.updateCar
+    handler: carController.updateCar,
+    schema: documentation.updateCarSchema
   },
   {
     method: 'DELETE',
     url: '/api/cars/:id',
-    handler: carController.deleteCar
+    handler: carController.deleteCar,
+    schema: documentation.deleteCarSchema
   }
 ]
 
